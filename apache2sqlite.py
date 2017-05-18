43b55981e16a6feb7223c06db407337dd5b28e5c
Rockvole <rockvole@gmail.com>
2017-05-18 16:23:13 -0700

    Add geoip database lookup

--- apache2sqlite.py
+++ apache2sqlite.py
@@ -2,6 +2,7 @@
 import sys
 import sqlite3
 import apache_log_parser
+import pygeoip
 from pprint import pprint
 
 def print_usage():
@@ -21,6 +22,7 @@ except ValueError:
 APACHE_COMBINED="%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\""  
 APACHE_COMMON="%h %l %u %t \"%r\" %>s %b"
 
+gi = pygeoip.GeoIP('/usr/share/GeoIP/GeoIP.dat')
 line_parser = apache_log_parser.make_parser(APACHE_COMBINED)
 
 conn = sqlite3.connect(db_name)
@@ -31,7 +33,7 @@ except sqlite3.OperationalError:
 	pass
 
 try:
-	c.execute("CREATE TABLE logs (id INTEGER PRIMARY KEY, source, request, status integer, size integer, method, referer, agent, version integer, date, user, label)")
+	c.execute("CREATE TABLE logs (id INTEGER PRIMARY KEY, source, request, status integer, size integer, method, referer, agent, version integer, date, user, label, country)")
 except sqlite3.OperationalError:
 	print("logs table already exists")
 
@@ -40,8 +42,8 @@ line_num = 1
 for line in f:
 	line_data = line_parser(line)
 	c.execute("   INSERT into logs "
-	          "               (id, source, request, status, size, method, referer, agent, version, date, user) "
-	          "        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+	          "               (id, source, request, status, size, method, referer, agent, version, date, user, country) "
+	          "        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
 	          (str(line_num),
 	           line_data['remote_host'],
 	           line_data['request_url'],
@@ -52,7 +54,8 @@ for line in f:
 	           line_data['request_header_user_agent'],
 	           line_data['request_http_ver'],
 	           line_data['time_received'],
-	           line_data['remote_user'])
+	           line_data['remote_user'],
+	           gi.country_name_by_addr(line_data['remote_host']))
 	          )
 	conn.commit()	
 		
